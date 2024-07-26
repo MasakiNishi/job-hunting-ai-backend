36447ed8a677b2e43f9a7414d8e2c317d2f89e59
alewon413 <alexander.wong@vanderbilt.edu>
2024-07-26 09:52:13 -0500

    -Added python script for converting user response to JSON -Added python script for general scraping of LinkedIn job posts with two generic query parameters (keywords, location)
    <!-- JIRA: [KAN-24](https://oregonstate-team-a4gvoaif.atlassian.net/browse/KAN-24) -->
    
    The motivation behind this is to jumpstart the process in verifying frontend-backend connection, as well as get started on our job scraping process with LinkedIn
    
    For user_response.py:
    -The frontend was initialized (npm start) and then the backend initialized with terminal command "python user_response.py"
    -Form was submitted from frontend and file was confirmed to receive info by checking in backend directory, under ./text_JSON/user_answers.json file
    
    For scrape_linkedin.py:
    -First verified job posting scraping via guest access and use of linkedin-api, and confirmed job posting access was available
    -Then initialized with terminal command "python scrape_linkedin.py"
    -Allow script to run, check backend directory under "./text_JSON/linkedin_jobs.json"
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    
    - [x] My code follows the code style of this project.
    - [x] I have commented my code, particularly in hard-to-understand areas.
    - [x] My changes generate no new warnings.
    - [ ] I have added tests to cover my changes.
    - [ ] I have passed lints.
    - [ ] All new and existing tests passed.
    
    I believe this is a relatively large change since it's a new addition, hopefully the comments provided might help clear up any confusion. However there could always be a better way, so feel free to make changes as necessary!

---
+++ user_response.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
+
+@app.route('/api/submit', methods=['POST'])
+def submit():
+    data = request.json
+
+    # Overwrite the JSON file with the received data
+    with open('./text_JSON/user_answers.json', 'w') as file:
+        json.dump(data, file, indent=4)
+    
+    return jsonify({"message": "Data saved successfully"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
